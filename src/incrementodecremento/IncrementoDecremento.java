ca85accf386e647cdc2ac13e06e5176a3e87a39d
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2021-08-05 11:41:54 -0600

    Initial commit

---
+++ src/incrementodecremento/IncrementoDecremento.java
@@ -0,0 +1,16 @@
+package incrementodecremento;
+
+public class IncrementoDecremento {
+
+    public static void main(String[] args) {
+      int x = 5;
+      
+      x += 1;
+      
+      System.out.println(x);
+      
+      double resultado = Math.random();
+      System.out.println(resultado);
+    }
+
+}
